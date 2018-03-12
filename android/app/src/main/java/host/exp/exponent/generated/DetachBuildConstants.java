55fd073af2e053d9458d8325588eca43ff2fde6b
Chung Poon <cmpoon@users.noreply.github.com>
2018-03-12 23:12:57 +0000

    Added ble and graphing svg libraries

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp9c65d520cee34cee8459ee39a1296a42://localhost:80";
 
 }
